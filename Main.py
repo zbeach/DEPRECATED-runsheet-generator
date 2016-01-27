1211c13620583825baf9a319aabf4d826fd44d7c
Zack Beach <z.beach@icloud.com>
2016-01-27 17:50:03 -0500

    Added column constants for list of shifts.

--- Main.py
+++ Main.py
@@ -8,13 +8,13 @@ with open('/Users/zack/Desktop/EXPORT.csv', 'r') as csvfile:
     HEADER_ROW = next(r)
 
     # Column indices from CSV file
-    LAST_NAME_COLUMN = HEADER_ROW.index("Employee Last Name")
-    FIRST_NAME_COLUMN = HEADER_ROW.index("Employee First Name")
-    POSITION_COLUMN = HEADER_ROW.index("Position Name")
-    CATEGORY_COLUMN = HEADER_ROW.index("Category")
-    START_TIME_COLUMN = HEADER_ROW.index("Start Time")
-    END_TIME_COLUMN = HEADER_ROW.index("End Time")
-    DATE_COLUMN = HEADER_ROW.index("Date")
+    LAST_NAME_CSV_COLUMN = HEADER_ROW.index("Employee Last Name")
+    FIRST_NAME_CSV_COLUMN = HEADER_ROW.index("Employee First Name")
+    POSITION_CSV_COLUMN = HEADER_ROW.index("Position Name")
+    CATEGORY_CSV_COLUMN = HEADER_ROW.index("Category")
+    START_TIME_CSV_COLUMN = HEADER_ROW.index("Start Time")
+    END_TIME_CSV_COLUMN = HEADER_ROW.index("End Time")
+    DATE_CSV_COLUMN = HEADER_ROW.index("Date")
 
     # Runsheet date
     inputDate = "1/28/2016" # Test
@@ -24,25 +24,39 @@ with open('/Users/zack/Desktop/EXPORT.csv', 'r') as csvfile:
 
     # Add all shifts from date to queue
     for row in r:
-        if row[DATE_COLUMN] == inputDate:
+        if row[DATE_CSV_COLUMN] == inputDate:
             # Only add shifts that are not Operations Supervisor or Mega Bus Connect
-            if (row[POSITION_COLUMN] != "Operations Supervisor") and (row[POSITION_COLUMN] != "Mega Bus Connect"):
+            if (row[POSITION_CSV_COLUMN] != "Operations Supervisor") and (row[POSITION_CSV_COLUMN] != "Mega Bus Connect"):
                     allShifts.put(row)
 
     # Create list of shifts for runsheet
+    # Set column constants
+    LAST_NAME_COLUMN = 0
+    FIRST_NAME_COLUMN = 1
+    POSITION_COLUMN = 2
+    START_TIME_COLUMN = 3
+    END_TIME_COLUMN = 4
+    CATEGORY_COLUMN = 5
+
+    # Initialize with default value of "" for every element
     shifts = [["" for x in range(6)] for x in range(allShifts.qsize())]
     for i in shifts:
         current = allShifts.get()
-        i[0] = current[LAST_NAME_COLUMN]
-        i[1] = current[FIRST_NAME_COLUMN]
-        i[2] = current[POSITION_COLUMN]
-        i[3] = current[START_TIME_COLUMN]
-        i[4] = current[END_TIME_COLUMN]
-        i[5] = current[CATEGORY_COLUMN]
-        print(i);
+        i[LAST_NAME_COLUMN] = current[LAST_NAME_CSV_COLUMN]
+        i[FIRST_NAME_COLUMN] = current[FIRST_NAME_CSV_COLUMN]
+        i[POSITION_COLUMN] = current[POSITION_CSV_COLUMN]
+        i[START_TIME_COLUMN] = current[START_TIME_CSV_COLUMN]
+        i[END_TIME_COLUMN] = current[END_TIME_CSV_COLUMN]
+        i[CATEGORY_COLUMN] = current[CATEGORY_CSV_COLUMN]
 
     # Sort shifts for runsheet presentation
+    temp = shifts[:] # temp is initalized as a copy of shifts
+
+
     # Add category separators
     # Generate XLSX
+
+
+
 
 
